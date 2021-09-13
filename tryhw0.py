7d5bbbfab4ab313c12162724d4796439c4f177e7
share424 <share424@gmail.com>
2021-09-14 02:30:21 +0800

    change output directory

--- tryhw0.py
+++ tryhw0.py
@@ -5,23 +5,23 @@ im = load_image("data/dog.jpg")
 for row in range(im.h):
     for col in range(im.w):
         set_pixel(im, col, row, 0, 0)
-save_image(im, "dog_no_red")
+save_image(im, "output/dog_no_red")
 
 # 3. Grayscale image
 im = load_image("data/colorbar.png")
 graybar = rgb_to_grayscale(im)
-save_image(graybar, "graybar")
+save_image(graybar, "output/graybar")
 
 # 4. Shift Image
 im = load_image("data/dog.jpg")
 shift_image(im, 0, .4)
 shift_image(im, 1, .4)
 shift_image(im, 2, .4)
-save_image(im, "overflow")
+save_image(im, "output/overflow")
 
 # 5. Clamp Image
 clamp_image(im)
-save_image(im, "doglight_fixed")
+save_image(im, "output/doglight_fixed")
 
 # 6. Colorspace and saturation
 im = load_image("data/dog.jpg")
@@ -29,7 +29,7 @@ rgb_to_hsv(im)
 shift_image(im, 1, .2)
 clamp_image(im)
 hsv_to_rgb(im)
-save_image(im, "dog_saturated")
+save_image(im, "output/dog_saturated")
 
 # 7. scale saturation
 im = load_image("data/dog.jpg")
@@ -37,4 +37,4 @@ rgb_to_hsv(im)
 scale_image(im, 1, 2)
 clamp_image(im)
 hsv_to_rgb(im)
-save_image(im, "dog_scale_saturated")
+save_image(im, "output/dog_scale_saturated")
